7ee30ecd8667060727348932f61db05e855d9bec
Deepakkzde <95161019+Deepakkzde@users.noreply.github.com>
2021-12-11 11:23:37 +0530

    Add files via upload

---
+++ CreateLead.java
@@ -0,0 +1,19 @@
+package Week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateLead {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://leafground.com/pages/Dropdown.html");
+		driver.manage().window().maximize();
+String title = driver.getTitle();	
+	System.out.println(title);
+
+	}
+}
